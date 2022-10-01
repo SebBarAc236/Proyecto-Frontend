5fc964243b7715ebc2a0d60bb5df3989432ef65b
Sebastian Barrios <20190222@aloe.ulima.edu.pe>
2022-09-30 21:34:52 -0500

    completado de estilo en inicio

--- src/components/INICIO/login.jsx
+++ src/components/INICIO/login.jsx
@@ -5,24 +5,37 @@ import './loginstyle.css'
 const Login = (props) => {
 
     return <div>
-        <Header/>
-        <div id='bordescontenedorR'>
-        <div id='ignore'>.</div>
-        <div id='contenedorR'>
-            <div id='tituloContenedorR'>LOG IN.</div>
-            <div id='textoContenedorR'>Log in with your details below to view your order.</div>
-            <input type="text" class="form-control" id="floatingInput" placeholder="Email"/>
-            <input type="password" class="form-control" id="floatingInput" placeholder="Password"/>
-            
-            <Link to={"/"}>
-                <a id='botonblanco' class="btn btn-primary" role="button">LOGIN</a>
-            </Link>
-            <div><a href=" ">Forgot your password?</a></div>
-            <div id='ignore'>.</div>
-            <div id='textoContenedorR'>Don't have an account?</div>
-            <Link to={"/Register"}>
-                <a id='botonblanco' class="btn btn-primary">SIGN UP</a>
-            </Link>
+        <div className='container'>
+            <div className='row'><Header/></div>
+            <div className='row' id='ignore'>.</div>
+            <div className='row'>
+            <div className='col'></div>
+            <div  className='col-4'   id='contenedorR'>
+                <div id='ignore'>.</div>
+                <div id='tituloContenedorR'>LOG IN.</div>
+                <div id='textoContenedorR'>Log in with your details below to view your order.</div>
+                <div id='ignore'>.</div>
+                <input type="text" class="form-control" id="floatingInput" placeholder="Email"/>
+                <div id='ignore'>.</div>
+                <input type="password" class="form-control" id="floatingInput" placeholder="Password"/>
+                <div id='ignore'>.</div>
+                <Link to={"/"}>
+                <div class="d-grid gap-2">
+                    <button id='botonblanco' class="btn btn-primary" type="button">LOGIN</button>
+                </div>
+                </Link>
+                <div><a href=" ">Forgot your password?</a></div>
+                <div id='ignore'>.</div>
+                <div id='textoContenedorR'>Don't have an account?</div>
+                <Link to={"/Register"}>
+                    <div class="d-grid gap-2">
+                        <button id='botonblanco' class="btn btn-primary" type="button">SIGN UP</button>
+                    </div>
+                </Link>
+                <div id='ignore'>.</div>
+            </div>
+            <div className='col'></div>
+                  
         </div>
         </div>
     </div>
--- src/components/INICIO/register.jsx
+++ src/components/INICIO/register.jsx
@@ -5,17 +5,35 @@ import './loginstyle.css'
 const Register = (props) => {
 
     return <div>
-        <Header/>
-        <div id='contenedorR'>
-            <div>CREATE ACCOUNT</div>
-            <input type="text" class="form-control" id="floatingInput" placeholder="First Name"/>
-            <input type="text" class="form-control" id="floatingInput" placeholder="Last Name"/>
-            <input type="text" class="form-control" id="floatingInput" placeholder="Email"/>
-            <input type="password" class="form-control" id="floatingInput" placeholder="Password"/>
-            <Link to={"/"}>
-                <button>CREATE</button>
-            </Link>
-            <div> Already registered? Try <Link to={"/Login"}>sign in</Link></div>
+        <div className='container'>
+            <div className='row'><Header/></div>
+            <div className='row' id='ignore'>.</div>
+            <div className='row'>
+            <div className='col'></div>
+            <div  className='col-4'   id='contenedorR'>
+                <div id='ignore'>.</div>
+                <div id='tituloContenedorR'>CREATE ACCOUNT</div>
+                <div id='ignore'>.</div>
+                <input type="text" class="form-control" id="floatingInput" placeholder="First Name"/>
+                <div id='ignore'>.</div>
+                <input type="text" class="form-control" id="floatingInput" placeholder="Last Name"/>
+                <div id='ignore'>.</div>
+                <input type="text" class="form-control" id="floatingInput" placeholder="Email"/>
+                <div id='ignore'>.</div>
+                <input type="password" class="form-control" id="floatingInput" placeholder="Password"/>
+                <div id='ignore'>.</div>
+                <Link to={"/"}>
+                <div class="d-grid gap-2">
+                    <button id='botonblanco' class="btn btn-primary" type="button">CREATE</button>
+                </div>
+                </Link>
+                <div>Already registered? Try <Link to={"/Login"}><a>sign in</a></Link></div>
+                <div id='ignore'>.</div>
+                <div id='ignore'>.</div>
+            </div>
+            <div className='col'></div>
+                  
+        </div>
         </div>
     </div>
    
