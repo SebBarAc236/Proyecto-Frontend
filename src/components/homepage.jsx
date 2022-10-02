794e496f3f4feda248e5ff44be4e93c9fae730f3
Jannire Trujillo <Janniretruji@gmail.com>
2022-10-01 22:51:59 -0500

    footer

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -7,7 +7,7 @@ const Home = (props) => {
 
     return <div>
         <Header/>
-        <div className="p-5">
+        <div className="ps-5 mb-5" style={{height: "77vh"}}>
             <img id="controller" className="m-3" src="https://cdn-icons-png.flaticon.com/512/2540/2540374.png" alt="Video-game Controller" />
             <h1 id="pc" className="text-white p-1">Build your PC!</h1>
             <h2 id="subpc" className="p-1">Just what you need</h2>
@@ -26,7 +26,39 @@ const Home = (props) => {
             </span>
         </div>
         <footer id="foot">
-            <p>holas</p>
+            <div className="container">
+                <div className="row p-2">
+                    <div className="col">
+                        <b>Sign up to out newsletter for the latest PC news.</b>
+                        <div>
+                            <input id="email" type="text" placeholder="Email" className=""/>
+                            <button id="subs" className="btn rounded border border-success m-1 p-2 border-opacity-10">
+                                SUBSCRIBE
+                            </button>
+                        </div>
+                        <div>
+                            <img className="fb ms-3" src="https://cdn-icons-png.flaticon.com/512/747/747543.png" alt="facebook" />
+                            <img className="fb ms-3" src="https://cdn-icons-png.flaticon.com/512/3536/3536424.png" alt="twitter" />
+                            <img className="fb ms-3" src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="twitter" />
+                        </div>
+                    </div>
+                    <div className="col">
+                        <p>Build your PC</p>
+                        <p>Why Redux</p>
+                        <p>Support</p>
+                    </div>
+                    <div className="col">
+                        <p>Good</p>
+                        <p>Better</p>
+                        <p>Best</p>
+                    </div>
+                    <div className="col">
+                        <p>Terms and conditions</p>
+                        <p>Privacy Policy</p>
+                        <p>Refund Policy</p> 
+                    </div>
+                </div>
+            </div>
         </footer>
     </div>
         
