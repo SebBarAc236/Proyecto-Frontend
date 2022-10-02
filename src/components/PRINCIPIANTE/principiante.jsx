c9c30eb09d8ad5120f5e38c8fc20b819819364d7
FrancoArriaran <franco7arriaran@gmail.com>
2022-10-02 01:58:01 -0500

    cambios a principiante

--- src/components/PRINCIPIANTE/principiante.jsx
+++ src/components/PRINCIPIANTE/principiante.jsx
@@ -1,11 +1,12 @@
-import Header from '../header_todos';
+import Header from './header_todos';
 import { Link } from "react-router-dom"
+import mando1 from './IMAGENES-F/mando1.png'
 
 
 const Principiante = () => {
     return <div>
             <div>
-            <span id="titulo" className="texto" style="color: white; font-size: 35px;">
+            <span id="titulo" className="texto" style={{color: "white", fontsize: "35px"}}>
                 <b>
                     What do you need?
                 </b>
@@ -28,13 +29,13 @@ const Principiante = () => {
         
             </span>
             <span ALIGN="RIGHT">
-                <button className="rounded border-0" style="height:50px ; width:150px">
+                <button className="rounded border-0" style={{height: "50px" , width: "150px"}}>
                     Back
                 </button>
                 <span>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 </span>
-                <button className="rounded border-0" style="height:50px ; width:150px; background-color: rgba(195,90,234,255);" >
+                <button className="rounded border-0" style={{height: "50px" , width: "150px", backgroundcolor: "rgba(195,90,234,255)"}} >
                     Next
                 </button>
             </span>
@@ -52,62 +53,15 @@ const Principiante = () => {
             <div className="col" id= "contentcol"  >
                 <button id="boton" type="button" className="bg-secondary rounded border-0" 
                     >
-                    <img src="/mando2.png" width="110" height="50" />
-                    <div className="Boton" style="color: white;">
+                    <img src={mando1} style={{width:"110", height:"50"}} />
+                    <div className="Boton" style={{color: "white"}}>
                         <b> Gaming</b>
                     </div>
-                </button>
-            </div>
-            <div className="col" id= "contentcol" >
-                <button id="boton" type="button" className="bg-secondary rounded border-0" 
-                   >
-                    <img src="/design.png" width="110" height="50" />
-                    <div className="Boton" style="color: white;">
-                        <b> Design</b>
+                    </button>
                     </div>
-                </button>
-            </div>
-            <div className="col" id= "contentcol" style="margin-right: 350px;">
-                <button id="boton" type="button" className="bg-secondary rounded border-0" 
-                >
-                <img src="codingg.png" width="110" height="50" />
-                <div className="Boton" style="color: white;">
-                    <b> Coding</b>
-                </div>
-            </button>
-            </div>
-          
-        </div>
-        <div className="row" id="contentrow">
-            <div className="col" id= "contentcol" style="height: 100%;">
-                <button id="boton" type="button" className="bg-secondary rounded border-0" 
-                >
-                <img src="rendering.png" width="110" height="50"/>
-                <div className="Boton" style="color: white;">
-                    <b> Rendering</b>
-                </div>
-            </button>
-            </div>
-            <div className="col" id= "contentcol" >
-                <button id="boton" type="button" className="bg-secondary rounded border-0" 
-                >
-                <img src="officefinal.png" width="110" height="50" />
-                 <div className="Boton" style="color: white;">
-                        <b> Office</b>
                     </div>
-            </button>
-            </div>
-            <div className="col" id= "contentcol" style="margin-right: 350px;">
-                <button id="boton" type="button" className="bg-secondary rounded border-0" 
-                >
-                <img src="other.png" width="110" height="50" />
-                <div className="Boton" style="color: white;">
-                    <b> Other</b>
-                </div>
-            </button>
-            </div>
-
-    </div>
-    </div>
+                    </div>
+                    
+               
 }
 export default Principiante;
