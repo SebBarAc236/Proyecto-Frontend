308d4f64380f8ced3a723167836734c1cdcaf71f
FrancoArriaran <franco7arriaran@gmail.com>
2022-10-02 03:18:37 -0500

    termino de pagina de principiante

--- src/components/PRINCIPIANTE/principiante.jsx
+++ src/components/PRINCIPIANTE/principiante.jsx
@@ -1,6 +1,10 @@
 import Header from './header_todos';
 import { Link } from "react-router-dom"
 import mando1 from './IMAGENES-F/mando1.png'
+import design1 from './IMAGENES-F/design1.png'
+import rendering from './IMAGENES-F/rendering1'
+import office from './IMAGENES-F/office'
+import other1 from './IMAGENES-F/other1'
 
 
 const Principiante = () => {
@@ -53,14 +57,68 @@ const Principiante = () => {
             <div className="col" id= "contentcol"  >
                 <button id="boton" type="button" className="bg-secondary rounded border-0" 
                     >
-                    <img src={mando1} style={{width:"110", height:"50"}} />
+                    <img src={mando1} style={{width:"110px", height:"50px"}} />
                     <div className="Boton" style={{color: "white"}}>
                         <b> Gaming</b>
+                        </div>
+                </button>
+            </div>
+            <div class="col" id= "contentcol" >
+                <button id="boton" type="button" class="bg-secondary rounded border-0" 
+                   >
+                    <img src={design1} style={{width:"110px", height:"50px"}}/>
+                    <div class="Boton" style={{color: "white"}}>
+                        <b> Design</b>
                     </div>
-                    </button>
+                </button>
+            </div>
+            <div class="col" id= "contentcol" style={{width:"110%", height:"50%"}}>
+                <button id="boton" type="button" class="bg-secondary rounded border-0" 
+                >
+                <img src={coding} style={{width:"110px", height:"50px"}} />
+                <div class="Boton" style={{color: "white"}}>
+                    <b> Coding</b>
+                </div>
+            </button>
+            </div>
+          
+        </div>
+        <div class="row" id="contentrow">
+            <div class="col" id= "contentcol" style={{width:"110%", height:"50%"}}>
+                <button id="boton" type="button" class="bg-secondary rounded border-0" 
+                >
+                <img src={rendering} style={{width:"110px", height:"50px"}} />
+                <div class="Boton" style={{color: "white"}}>
+                    <b> Rendering</b>
+                </div>
+            </button>
+            </div>
+            <div class="col" id= "contentcol" >
+                <button id="boton" type="button" class="bg-secondary rounded border-0" 
+                >
+                <img src={office} style={{width:"110", height:"50"}} />
+                 <div class="Boton" style={{color: "white"}}>
+                        <b> Office</b>
                     </div>
-                    </div>
-                    </div>
+            </button>
+            </div>
+            <div class="col" id= "contentcol" style={{marginright: "350px"}}>
+                <button id="boton" type="button" class="bg-secondary rounded border-0" 
+                >
+                <img src={other1} style={{width:"110", height:"50"}} />
+                <div class="Boton" style={{color: "white"}}>
+                    <b> Other</b>
+                </div>
+            </button>
+            </div>
+
+           
+
+        </div>
+
+       
+
+        </div>
                     
                
 }
