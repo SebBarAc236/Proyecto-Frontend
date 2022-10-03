340b8ae3953dbd9859638eb13693813903c8cad6
Sebastian Barrios <20190222@aloe.ulima.edu.pe>
2022-10-03 00:29:12 -0500

    terminado recomendaciones beginer

--- src/components/Recomendado.jsx
+++ src/components/Recomendado.jsx
@@ -6,7 +6,18 @@ import ryzenlogo from './imagenes/ryzenlogo.jpg'
 import aoruslogo from './imagenes/gigabytelogo.png'
 import corsairlogo from './imagenes/corsairlogo.png'
 import pcgamer from './imagenes/pcgamer.png'
-
+import windows from './imagen/windows11.png'
+import wireless from './imagen/wireless.png'
+import buildlogo from './imagen/build_logo.png'
+import coolermaster from './imagen/cooler_master.png'
+import corei7 from './imagen/core_i7.png'
+import masterliquid from './imagen/master_liquid.png'
+import motherboard from './imagen/motherboard.png'
+import powersupply from './imagen/power_supply.png'
+import qty from './imagen/QTY_CM.png'
+import ram from './imagen/RAM.png'
+import rtx from './imagen/RTX.png'
+import ssd from './imagen/SSD.png'
 const Recomendado = () => {
     return <div>
         <div className='row mx-auto' align="right"><Header/></div>
@@ -41,220 +52,220 @@ const Recomendado = () => {
         
         
         <div className='col mt-3 ms-3'>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={ryzenlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={corei7} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">AMD RYZEN 3600X</p>
+                        <p class="card-text">INTEL CORE I7-12700F 12-CORE</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        350$
+                        359$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={corsairlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={ram} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">CORSAIR VENGANCE 16GB RAM 8X2</p>
+                        <p class="card-text">16GB DDR4 DUAL CHANNEL</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        250$
+                        69$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={aoruslogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={masterliquid} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">GIGABYTE POWER SUPPLY 80PLUS GOLD</p>
+                        <p class="card-text">CM MASTERLIQUID ML240L</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        200$
+                        89$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={ryzenlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={coolermaster} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">AMD RYZEN 3600X</p>
+                        <p class="card-text">COOLER MASTER TD500 RGB</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        350$
+                        99$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={corsairlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={motherboard} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">CORSAIR VENGANCE 16GB RAM 8X2</p>
+                        <p class="card-text">ASUS PRIME B660-A | INTEL</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        250$
+                        149$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={aoruslogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={windows} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">GIGABYTE POWER SUPPLY 80PLUS GOLD</p>
+                        <p class="card-text">WINDOWS 11 HOME + USB RECOVERY</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        200$
+                        120$
                     </div>
                 </div>
                 </div>
             </div>
         </div>
         <div className='col mt-3 ms-3'>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={ryzenlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={rtx} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">AMD RYZEN 3600X</p>
+                        <p class="card-text">NVIDIA GEFORCE RTX 3070 8GB (VR READY)</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        350$
+                        679$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={corsairlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={ssd} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">CORSAIR VENGANCE 16GB RAM 8X2</p>
+                        <p class="card-text">1TB NVME M.2</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        250$
+                        99$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={aoruslogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={qty} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">GIGABYTE POWER SUPPLY 80PLUS GOLD</p>
+                        <p class="card-text">QTY 4X CM MASTERFANS RGB</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        200$
+                        29$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={ryzenlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={powersupply} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">AMD RYZEN 3600X</p>
+                        <p class="card-text">700W ATX 80 PLUS GOLD</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        350$
+                        79$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={corsairlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={wireless} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">CORSAIR VENGANCE 16GB RAM 8X2</p>
+                        <p class="card-text">WIRELESS 802.11AC</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        250$
+                        29$
                     </div>
                 </div>
                 </div>
             </div>
-            <div className='row'>
+            <div className='row' style={{height:"140px"}}>
                 <div class="card mb-3" id='listacompra'>
                 <div class="row g-0">
                     <div class="col-md-3">
-                        <img src={aoruslogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
+                        <img src={buildlogo} class="img-fluid rounded-start mt-2 mb-2" alt="..."/>
                     </div>
                     <div class="col-md-6">
                     <div class="card-body">
-                        <p class="card-text">GIGABYTE POWER SUPPLY 80PLUS GOLD</p>
+                        <p class="card-text">BUILD + SETUP + TESTING + WARRANTY</p>
                     </div>
                     </div>
                     <div class="col-md-3">
                         <div>&nbsp;</div>
-                        200$
+                        99$
                     </div>
                 </div>
                 </div>
